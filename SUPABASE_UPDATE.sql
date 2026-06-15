067a5db98ea08f2704e2fe2b633c6d6534e78d10
B Ntshumayelo <bntshumayelob@gmail.com>
2026-06-15 22:05:02 +0200

    Delete SUPABASE_UPDATE.sql

--- SUPABASE_UPDATE.sql
+++
@@ -1,3 +0,0 @@
--- Run this in Supabase SQL Editor to add doc_verified column
-ALTER TABLE public.applications ADD COLUMN IF NOT EXISTS doc_verified boolean DEFAULT false;
-ALTER TABLE public.applications ADD COLUMN IF NOT EXISTS email text;
